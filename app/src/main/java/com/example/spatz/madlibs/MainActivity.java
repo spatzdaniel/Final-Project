0640e883eeeabe6e1edcf6eb1a10b4b482629a04
spatzdaniel <dspatz2@illinois.edu>
2018-12-03 14:43:27 -0600

    Removed last games thing. Changed teh way for inputs to work. Need to figure out how to turn the xml data into java variables and vice versa.

--- app/src/main/java/com/example/spatz/madlibs/MainActivity.java
+++ app/src/main/java/com/example/spatz/madlibs/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.spatz.madlibs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
     }
+    private String[] inputsNeeded = new String[]{"noun", "verb", "adjective"};
+
+    public void libTime(View view) {
+
+        setContentView(R.layout.inputforthelib);
+    }
+
 }
