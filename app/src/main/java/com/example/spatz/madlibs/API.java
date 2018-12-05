6fb52c4cff744655c11d848b1306270287895f16
Benedict Austriaco <bva2@illinois.edu>
2018-12-05 11:02:59 -0600

    In case changing the app module didn't save.

--- app/src/main/java/com/example/spatz/madlibs/API.java
+++ app/src/main/java/com/example/spatz/madlibs/API.java
@@ -1,4 +1,19 @@
 package com.example.spatz.madlibs;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class API {
+    private String url;
+    private String quote;
+    private String emptyLib;
+    API() {
+
+    }
 }
